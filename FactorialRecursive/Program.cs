386481da76fa277723dd5586c7fbefbcdb9a01ac
Victor Pavel <victor.pavel25@gmail.com>
2018-04-02 18:19:24 +0100

    Add project files.

---
+++ FactorialRecursive/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FactorialRecursive
+{
+    class Factorial {
+        public int getFactorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * getFactorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Factorial fac = new Factorial();
+            Console.WriteLine("factorial of 5 is: {0}", fac.getFactorial(5));
+            Console.WriteLine("factorial of 6 is: {0}", fac.getFactorial(6));
+            Console.ReadLine();
+        }
+    }
+}
+
